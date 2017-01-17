5e296151979206759cc63293910f91927a2fe50e
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2017-01-17 10:29:18 +0000

    loading from  stash number rather than full code, and returning None if variable not found

--- libs/load_stash.py
+++ libs/load_stash.py
@@ -1,14 +1,27 @@
 import iris
+import sys
+import warnings
 from pdb import set_trace as browser
 
 def load_stash(files, code, name, units = None):
     print name
     print code
+    try:
+        codeNum = int(code)
+        if   (len(code) == 5): code = 'm01s' + code[0:2] + 'i' + code[2:]
+        elif (len(code) == 7): code = 'm' + code[0:2] + 's' + code[2:4] + 'i' + code[4:]
+    except:
+        pass
 
+    
     stash_constraint = iris.AttributeConstraint(STASH = code)
     
-    cube = iris.load_cube(files, stash_constraint)
-    cube.var_name = name
-    cube.standard_name = None
-    if (units is not None): cube.units = units
-    return cube    
+    try:
+        cube = iris.load_cube(files, stash_constraint)
+        cube.var_name = name
+        cube.standard_name = None
+        if (units is not None): cube.units = units
+        return cube
+    except:    
+        warnings.warn('unable to open variable: ' + code)
+        pass  
