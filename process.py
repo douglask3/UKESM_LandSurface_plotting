ae363ab6675c5eeb357b10d8082703302e031d41
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-02-01 22:01:19 +0000

    semi-sudo coding for multi suit comparisons

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import sys
 from   pdb   import set_trace as browser
 from   libs.grab_data import *
 from   libs.ConfigGet import ConfigGet
+import warnings
 
 Config = ConfigGet(sys.argv[1])
 
@@ -12,12 +13,16 @@ if (ceh): import libs.import_iris
 from   libs.open_plot_return import *
 
 datDir       = Config.Default("FileInfo", "dir"         )
-job          = Config.Default("FileInfo", "job"         )
+jobs         = Config.Default("FileInfo", "job"         , asList = True)
 jdir         = Config.Default("FileInfo", "subDir"      )
 stream       = Config.Default("FileInfo", "Stream"      )
 grab         = Config.Default("FileInfo", "grab"        , True,  "boolean")
 running_mean = Config.Default("FileInfo", "running_mean", False, 'boolean')
 
+datDir = []
+## track darDirs
+for job in jobs:
+## Indent from here
 if (datDir is None):
     if (job is None or stream is None):
         sys.exit('either a local dir for ESM output, or a suite code and ouput stream need to be defined')
@@ -30,7 +35,9 @@ if (datDir is None):
         if (newStash is not None): stash.extend(newStash)
     
     if (grab): grab_data(job, stream, stash, datDir)    
-
+## TO here
+	
+	
 for section in Config.sections():
     if (section == 'FileInfo' or section == 'MachineInfo'): continue
     
@@ -39,18 +46,27 @@ for section in Config.sections():
     FigUnits      =  Config.Default(section, "FigUnits"                        )
     FigCmap       =  Config.Default(section, "FigCmap"      , "brewer_Greys_09")
     VarStashCodes =  Config.Default(section, "VarStashCodes", "required"       , asList = True)
-    VarNames      =  Config.Default(section, "VarNames"     , VarStashCodes    , asList = True)
+	
+	if (len(jobs) > 1 and len(VarStashCodes) > 1):
+		warnings.warn('More than one stash code for a multi-job plot. Only first one uses')
+		VarStashCodes = [VarStashCodes[0]]
+	
+	VarNames_default = jobs if (len(jobs) > 1) else VarStashCodes
+	
+    VarNames      =  Config.Default(section, "VarNames"     , VarNames_default    , asList = True)
     VarScaling    =  Config.Default(section, "VarScaling"   , 1.0  , "float"   )
     Total         =  Config.Default(section, "Total"        , False, "boolean" )
     Stream        =  Config.Default(section, "Stream"                          )
     FigTSMean     =  Config.Default(section, "FigTSMean"    , True , "boolean" )
     
-    if (Stream is not None): datDirt = datDir + Stream + '/'
-    else: datDirt = datDir
-        
-    files = sort(listdir_path(datDirt))
+	files = []
+	for datD in datDir:
+		datDirt = datDir if Stream is None else datDir + Stream + '/'
+		files.append = [sort(listdir_path(i)) for i in datDir]
     
     FigName = jdir + '/' + FigName
+	
+	## adapt for multi files jobs
     opr   = open_plot_return(files, total = Total, running_mean = running_mean)
     opr.open_plot_and_return(FigName, FigTitle, VarStashCodes, VarNames,  FigUnits, FigTSMean, FigCmap, scale = VarScaling)
 
