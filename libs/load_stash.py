a3a1e716e73f50c83bd0f2620d20a9e65a47fe41
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2017-10-04 17:50:39 +0100

    correcitng some problem by moveing coord range extract to class

--- libs/load_stash.py
+++ libs/load_stash.py
@@ -5,50 +5,50 @@ import warnings
 import numpy as np
 from pdb import set_trace as browser
 
-def coordRangeExtract(dat, lon = None, lat = None, point = None, point_as_ij = False):
-    def coordRange2List(c, r):
-        if c is not None:
-            if  isinstance(c, list) and  len(c) == 1: c = c[0]
-        return c    
+class coordRangeExtract(object):
+    def __init__(self, dat, lon = None, lat = None, point = None, point_as_ij = False):
+        def coordRange2List(c, r):
+            if c is not None:
+                if  isinstance(c, list) and  len(c) == 1: c = c[0]
+            return c    
   
-    if point is not None and point != "":
-        latlon = point.split(';')  
-        latlon = [i.split(':') for i in latlon]
-        lon, lat = [[float(j) for j in i] for i in latlon]
-        if point_as_ij:                
-            lon = dat.coord('longitude').points[lon][0]
-            lat = dat.coord('latitude' ).points[lat][0]
-            dat.long_name += '-' + 'ijs:'
-        dat.long_name += '-' + point
+        if point is not None and point != "":
+            latlon = point.split(';')  
+            latlon = [i.split(':') for i in latlon]
+            lon, lat = [[float(j) for j in i] for i in latlon]
+            if point_as_ij:                
+                lon = dat.coord('longitude').points[lon][0]
+                lat = dat.coord('latitude' ).points[lat][0]
+                dat.long_name += '-' + 'ijs:'
+            dat.long_name += '-' + point
        
-    self.lon = coordRange2List(lon, [-180, 180])
-    self.lat = coordRange2List(lat, [-90 ,  90])
+        self.lon = coordRange2List(lon, [-180, 180])
+        self.lat = coordRange2List(lat, [-90 ,  90])
 
-    if isinstance(self.lon, list):
-        def lonRange(cell): return self.lon[0] <= cell <= self.lon[1]
-    else:
-        def lonRange(cell): return cell == self.lon
+        if isinstance(self.lon, list):
+            def lonRange(cell): return self.lon[0] <= cell <= self.lon[1]
+        else:
+            def lonRange(cell): return cell == self.lon
 
-    if isinstance(self.lat, list):
-        def latRange(cell): return self.lat[0] <= cell <= self.lat[1]
-    else:
-        def latRange(cell): return cell == self.lat
+        if isinstance(self.lat, list):
+            def latRange(cell): return self.lat[0] <= cell <= self.lat[1]
+        else:
+            def latRange(cell): return cell == self.lat
 
-    try: dat.coord('latitude' ).guess_bounds()
-    except: pass
-    try: dat.coord('longitude').guess_bounds() 
-    except: pass
+        try: dat.coord('latitude' ).guess_bounds()
+        except: pass
+        try: dat.coord('longitude').guess_bounds() 
+        except: pass
         
-    if self.lon is not None: dat = dat.extract(iris.Constraint(longitude = lonRange))
-    if self.lat is not None: dat = dat.extract(iris.Constraint(latitude  = latRange))
-    try:
-        if self.lat[0] == self.lat[1] and self.lon[0] == self.lon[1]:
-            if dat.coord('latitude').shape != (1,) or dat.coord('longitude').shape != (1,):
-                dat = [i.collapsed(['latitude', 'longitude'], iris.analysis.MEAN) for i in dat]
-    except:
-        pass
-
-    return dat
+        if self.lon is not None: dat = dat.extract(iris.Constraint(longitude = lonRange))
+        if self.lat is not None: dat = dat.extract(iris.Constraint(latitude  = latRange))
+        try:
+            if self.lat[0] == self.lat[1] and self.lon[0] == self.lon[1]:
+                if dat.coord('latitude').shape != (1,) or dat.coord('longitude').shape != (1,):
+                    dat = [i.collapsed(['latitude', 'longitude'], iris.analysis.MEAN) for i in dat]
+        except:
+            pass
+        self.dat = dat
 
 class load_stash(object):
 
@@ -62,6 +62,7 @@ class load_stash(object):
             if months is not None: self.extractMonths(months)
             elif climatology: self.convert2Climatology()
             if soillvs is not None:
+                browser()
                 if isinstance(soillvs, int): self.dat = self.stash_levels(soillvs, 'soil_model_level_number')
                 else: self.dat = [self.stash_levels(soillv, 'soil_model_level_number') for soillv in soillvs]
             if lbelvs is not None:
@@ -93,11 +94,13 @@ class load_stash(object):
                 self.dat.var_name  = varname
                 self.dat.long_name = longname
             
-            if isinstance(self.self, list):     
-                self.dat = [coordRange(i, lon, lat, point, point_as_ij) for i in self.dat]
+            if isinstance(self.dat, list):     
+                self.dat = [coordRangeExtract(i, lon, lat, point, point_as_ij).dat for i in self.dat]
             else:
-                self.dat = coordRange(dat, lon, lat, point, point_as_ij)
-
+                try:
+                    self.dat = coordRangeExtract(self.dat, lon, lat, point, point_as_ij).dat
+                except:
+                    browser()
 
                 
     def stash_code(self, files, code):    
